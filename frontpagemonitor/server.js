400783836d4884b0e022dd3cd934c399f34d50dc
Tillson Galloway <tillson.galloway@gmail.com>
2015-04-03 21:18:10 -0400

    Add a youtube view counter to frontpagemonitor

--- frontpagemonitor/server.js
+++ frontpagemonitor/server.js
@@ -1,8 +1,11 @@
 var express = require('express');
-var client = require('twilio')('other key', 'api key');
+var client = require('twilio')('account id', 'secret');
 var http = require('http');
 var app = express();
-var currentPlace = -1
+
+var currentPlace = -1;
+var currentViews = -1;
+var nextMilestone = -1;
 
 var redditFrontPage = {
   host: "www.reddit.com",
@@ -11,16 +14,55 @@ var redditFrontPage = {
   method: 'GET'
 };
 
+var youtubeVideoAPILink = {
+  host: "gdata.youtube.com",
+  port: 80,
+  path: '/feeds/api/videos/GXTLVQ1eiik?v=2&alt=json',
+  method: 'GET'
+};
+
 function sendMessage(number, msg) {
   client.sendMessage({
     to: number, 
-    from: '+number',
+    from: '+12014398113',
     body: msg
   }, function(err, responseData) {
+    // bleh
   });
 }
 
-setInterval(checkFrontPage, 1000 * 10)
+setInterval(checkYoutube, 1000 * 120)
+
+function checkYoutube() {
+  var req = http.request(youtubeVideoAPILink, function(res) {
+    var data = ""
+    res.on('data', function(d) {
+      data += d;
+    });
+    res.on("end", function() {
+        var json = JSON.parse(data);
+        var entry = json["entry"];
+        var views = entry["yt$statistics"]["viewCount"];
+        if (views > nextMilestone && nextMilestone != -1) {
+          console.log("Passed the milestone of " + nextMilestone + ".  (" + (new Date()) + ")");
+
+          sendMessage("+1blahblahblah", "The Julian video just passed " + nextMilestone + " views. (" + views + ")");
+
+          nextMilestone = nextMilestone += 10000;
+          console.log("Next milestone: " + nextMilestone);
+        }
+        if (views != currentViews && currentViews != -1) {
+          console.log("View count updated. Now at " + views);
+        }
+        currentViews = views;
+        if (nextMilestone == -1) {
+          nextMilestone = Math.ceil(views / 100000) * 100000;
+          console.log("Lets get started... Setting the milestone to " + nextMilestone);
+        }
+    })
+  });
+  req.end();
+}
 
 function checkFrontPage() {
   var req = http.request(redditFrontPage, function(res) {
@@ -56,5 +98,5 @@ function checkFrontPage() {
   req.end();
 }
 
-checkFrontPage();
+checkYoutube();
 app.listen(8080);
